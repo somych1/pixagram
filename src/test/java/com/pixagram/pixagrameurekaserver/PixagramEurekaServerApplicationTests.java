107eea1ee92b31c9acc4411ac11a3bc941440002
samat <somych1@gmail.com>
2022-10-28 17:53:14 -0500

    eureka server up and running

---
+++ src/test/java/com/pixagram/pixagrameurekaserver/PixagramEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixagram.pixagrameurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixagramEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
